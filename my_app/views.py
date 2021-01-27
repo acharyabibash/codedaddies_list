61e656966b8b6419867ad7f642de4378daee39ad
acharyabibash <acharabibash@outlook.com>
2021-01-27 21:58:15 +0545

    1.53.02

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,15 @@
 import requests
 
+from requests.compat import quote_plus 
+
+from . import models
+
 from django.shortcuts import render
 
 from bs4 import BeautifulSoup
+
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 # Create your views here.
 def home(request):
@@ -11,6 +18,12 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    # print(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    print(data)
     print(search)
     stuff_for_frontend = {
         'search':search,
